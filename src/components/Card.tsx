ce9c407959eea60d34737827ec48f963106fb703
Dan Ott <dan@dtott.com>
2023-06-30 11:06:44 -0400

    Add ability to have link in cta

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -79,16 +79,28 @@ Card.Description = function CardDescription({
   );
 };
 
-Card.Cta = function CardCta({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      aria-hidden="true"
-      className="relative z-10 mt-4 flex items-center text-sm font-medium text-primary-500 md:text-base lg:text-lg"
-    >
-      {children}
-      <ChevronRightIcon className="ml-1 h-4 w-4 stroke-current" />
-    </div>
-  );
+Card.Cta = function CardCta({
+  children,
+  href,
+}: {
+  children: React.ReactNode;
+  href?: LinkHref;
+}) {
+  const props = {
+    className:
+      'relative z-10 mt-4 flex items-center text-sm font-medium text-primary-500 md:text-base lg:text-lg',
+    children: (
+      <>
+        {children}
+        <ChevronRightIcon className="ml-1 h-4 w-4 stroke-current" />
+      </>
+    ),
+  };
+
+  if (href) {
+    return <Link href={href} {...props} />;
+  }
+  return <div aria-hidden="true" {...props} />;
 };
 
 Card.Eyebrow = function CardEyebrow<T extends React.ElementType = 'p'>({
