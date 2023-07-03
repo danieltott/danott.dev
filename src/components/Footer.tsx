a1569c6f88fdcf0665dd2de2fc19c640a907ea71
Dan Ott <dan@dtott.com>
2023-07-03 10:15:44 -0400

    header/footer remove sans

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ function NavLink({
 
 export function Footer() {
   return (
-    <footer className="mt-32 font-sans">
+    <footer className="mt-32">
       <Container.Outer>
         <div className="border-t border-stone-100 pb-16 pt-10 dark:border-stone-700/40">
           <Container.Inner>
