8de1477cb54f156d65288d659a8e44307e6a9f50
Dan Ott <dan@dtott.com>
2023-07-06 14:18:35 -0400

    fix switcher script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,41 @@ export const metadata: Metadata = {
   },
 };
 
+const modeScript = `
+let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+updateMode()
+darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
+window.addEventListener('storage', updateModeWithoutTransitions)
+
+function updateMode() {
+  let isSystemDarkMode = darkModeMediaQuery.matches
+  let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
+
+  if (isDarkMode) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+
+  if (isDarkMode === isSystemDarkMode) {
+    delete window.localStorage.isDarkMode
+  }
+}
+
+function disableTransitionsTemporarily() {
+  document.documentElement.classList.add('[&_*]:!transition-none')
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('[&_*]:!transition-none')
+  }, 0)
+}
+
+function updateModeWithoutTransitions() {
+  disableTransitionsTemporarily()
+  updateMode()
+}
+`;
+
 export default function RootLayout({
   children,
 }: {
@@ -62,42 +97,8 @@ export default function RootLayout({
       className={`h-full antialiased ${bodyFont.variable} ${freeLunch.variable}  font-serif`}
     >
       <head>
-        <Script id="darkmodetoggling">
-          {`
-  let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+        <script dangerouslySetInnerHTML={{ __html: modeScript }} />
 
-  updateMode()
-  darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
-  window.addEventListener('storage', updateModeWithoutTransitions)
-
-  function updateMode() {
-    let isSystemDarkMode = darkModeMediaQuery.matches
-    let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
-
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-
-    if (isDarkMode === isSystemDarkMode) {
-      delete window.localStorage.isDarkMode
-    }
-  }
-
-  function disableTransitionsTemporarily() {
-    document.documentElement.classList.add('[&_*]:!transition-none')
-    window.setTimeout(() => {
-      document.documentElement.classList.remove('[&_*]:!transition-none')
-    }, 0)
-  }
-
-  function updateModeWithoutTransitions() {
-    disableTransitionsTemporarily()
-    updateMode()
-  }
-`}
-        </Script>
         {process.env.NODE_ENV === 'production' && (
           <Script
             data-domain="dtott.com"
