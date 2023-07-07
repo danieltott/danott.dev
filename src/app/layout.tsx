827138bcdfab0095efe5ffa9ab73bf14ccbbdad8
Dan Ott <dan@dtott.com>
2023-07-07 12:36:19 -0400

    load starfield in iframe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Kreon } from 'next/font/google';
 import Stars from '@/components/Stars';
 import Script from 'next/script';
 import localFont from 'next/font/local';
+import StarFrame from '@/components/StarFrame';
 
 // Font files can be colocated inside of `app`
 const freeLunch = localFont({
@@ -94,7 +95,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`h-full antialiased ${bodyFont.variable} ${freeLunch.variable}  font-serif`}
+      className={`h-full antialiased ${bodyFont.variable} ${freeLunch.variable} font-serif`}
     >
       <head>
         <script dangerouslySetInnerHTML={{ __html: modeScript }} />
@@ -107,22 +108,7 @@ export default function RootLayout({
         )}
       </head>
       <body className="relative flex min-h-full flex-col bg-east-bay-50 dark:bg-east-bay-950">
-        <svg>
-          <defs>
-            <symbol id="starSvg" viewBox="0 0 24 24">
-              <path
-                strokeWidth={1}
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-              />
-            </symbol>
-          </defs>
-        </svg>
-
-        <Stars />
-        <Stars flip />
+        <StarFrame />
 
         <div className="fixed inset-0 z-0 flex justify-center sm:px-8">
           <div className="flex w-full max-w-7xl lg:px-8">
---
+++ src/components/StarFrame.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+export default function StarFrame() {
+  const pathname = usePathname();
+  return (
+    <iframe
+      src="/stars"
+      className="absolute inset-0 z-10 h-full w-full"
+      data-pathname={pathname}
+    />
+  );
+}
