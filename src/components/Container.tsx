d31fe4417bf88a13512139056f28c7fdee9e1b02
Dan Ott <dan@dtott.com>
2023-07-07 14:34:32 -0400

    make container full-height

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,8 +4,12 @@ import clsx from 'clsx';
 const OuterContainer = forwardRef<HTMLDivElement, React.ComponentProps<'div'>>(
   function OuterContainer({ className, children, ...props }, ref) {
     return (
-      <div ref={ref} className={clsx('sm:px-8', className)} {...props}>
-        <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
+      <div
+        ref={ref}
+        className={clsx('grid flex-1 sm:px-8', className)}
+        {...props}
+      >
+        <div className="mx-auto grid w-full max-w-7xl lg:px-8">{children}</div>
       </div>
     );
   }
@@ -16,10 +20,12 @@ const InnerContainer = forwardRef<HTMLDivElement, React.ComponentProps<'div'>>(
     return (
       <div
         ref={ref}
-        className={clsx('relative px-4 sm:px-8 lg:px-12', className)}
+        className={clsx('relative grid px-4 sm:px-8 lg:px-12', className)}
         {...props}
       >
-        <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+        <div className="mx-auto grid w-full max-w-2xl lg:max-w-5xl">
+          {children}
+        </div>
       </div>
     );
   }
