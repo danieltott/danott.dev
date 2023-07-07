39c52d3840814f95ea151de47c2d2eb9d762f1f5
Dan Ott <dan@dtott.com>
2023-07-07 09:34:34 -0400

    use use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,6 +107,20 @@ export default function RootLayout({
         )}
       </head>
       <body className="relative flex min-h-full flex-col bg-east-bay-50 dark:bg-east-bay-950">
+        <svg>
+          <defs>
+            <symbol id="starSvg" viewBox="0 0 24 24">
+              <path
+                strokeWidth={1}
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+              />
+            </symbol>
+          </defs>
+        </svg>
+
         <Stars />
         <Stars flip />
 
