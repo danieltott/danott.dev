8a9e4f7a24e1283fbf3a2520192830aa34295de2
Dan Ott <dan@dtott.com>
2023-07-07 13:09:04 -0400

    hide from sr

--- src/components/StarFrame.tsx
+++ src/components/StarFrame.tsx
@@ -6,6 +6,7 @@ export default function StarFrame() {
   const pathname = usePathname();
   return (
     <iframe
+      aria-hidden="true"
       src="/stars"
       className="absolute inset-0 z-10 h-full w-full"
       data-pathname={pathname}
