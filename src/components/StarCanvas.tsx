26e9eea9205a241ecc644db461f832e5eab6a880
Dan Ott <dan@dtott.com>
2023-07-12 17:42:45 -0400

    use real values

--- src/components/StarCanvas.tsx
+++ src/components/StarCanvas.tsx
@@ -4,7 +4,6 @@ import { useRef, useEffect, useState } from 'react';
 import {
   fillColors,
   random,
-  randomRotate,
   getRandom,
   randomScale,
 } from './star-util';
@@ -12,18 +11,28 @@ import { Bezier } from 'bezier-js';
 
 const STEP_RATIO = 0.02;
 
-function getCurve(steps: number, flip: boolean = false) {
-  const xLow = flip ? 14 : 0;
-  const xHigh = flip ? 100 : 86;
+function randomRotate() {
+  return (getRandom(0, 360) * Math.PI) / 180
+}
 
-  const startX = flip ? getRandom(86, 100) : getRandom(0, 14);
+function getCurve(steps: number, width:number,height:number, flip: boolean = false) {
+  // const xLow = flip ? 14 : 0;
+  // const xHigh = flip ? 100 : 86;
 
-  const lastPoint = { x: getRandom(xLow, xHigh), y: 50 };
+  const xBoxStart = flip ? (width * 7/12) : 0;
+  const xBoxEnd = flip ? width : (width * 5/12);
+
+  const xLow = xBoxStart + xBoxStart * .14;
+  const xHigh = flip ? width : xBoxEnd - xBoxEnd * .86;
+
+  const startX = flip ? getRandom(xBoxEnd - xBoxEnd * .14, xBoxEnd) : getRandom(0, xBoxStart + xBoxStart * .14);
+
+  const lastPoint = { x: getRandom(xLow, xHigh), y: height / 2 };
 
   const curve1 = new Bezier(
     { x: startX, y: 0 },
-    { x: getRandom(0, 50), y: getRandom(10, 40) },
-    { x: getRandom(50, 100), y: getRandom(10, 40) },
+    { x: getRandom(xBoxStart, xBoxEnd / 2), y: getRandom(height * .1, height * .4) },
+    { x: getRandom(xBoxEnd / 2, xBoxEnd), y: getRandom(height * .10, height * .40) },
     lastPoint
   );
 
@@ -31,9 +40,9 @@ function getCurve(steps: number, flip: boolean = false) {
 
   const curve2 = new Bezier(
     lastPoint,
-    { x: getRandom(0, 50), y: getRandom(60, 90) },
-    { x: getRandom(50, 100), y: getRandom(60, 90) },
-    { x: getRandom(xLow, xHigh), y: 100 }
+    { x: getRandom(xBoxStart, xBoxEnd / 2), y: getRandom(height * .60, height * .90) },
+    { x: getRandom(xBoxEnd / 2, xBoxEnd), y: getRandom(height * .60, height * .90) },
+    { x: getRandom(xLow, xHigh), y: height }
   );
 
   var LUT2 = curve2.getLUT(steps / 2);
@@ -50,14 +59,15 @@ export default function StarCanvas() {
       'M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
     );
     if (size) {
+      const {width,height} = size
       const canvas = canvasRef.current;
       if (canvas) {
         const ctx = canvas.getContext('2d');
 
         if (ctx) {
-          const steps = Math.floor(size.height * STEP_RATIO);
+          const steps = Math.floor(height * STEP_RATIO);
 
-          const curve = getCurve(steps);
+          const curve = [...getCurve(steps, width, height), ...getCurve(steps, width, height, true)];
 
           ctx.lineWidth = 1;
           ctx.strokeStyle = '#292524';
@@ -65,16 +75,18 @@ export default function StarCanvas() {
           ctx.lineJoin = 'round';
 
           curve.forEach((point) => {
-            const baseX = (point.x / 100) * ((size.width * 5) / 12);
-            const baseY = (point.y / 100) * size.height;
-            const scale = randomScale();
+            const {x:baseX, y:baseY} = point
+            // const baseX = (point.x / 100) * ((width * 5) / 12);
+            // const baseY = (point.y / 100) * height;
+            const scaleX = randomScale();
+            const scaleY = scaleX * getRandom(.7,1)
 
             ctx.resetTransform();
 
             ctx.translate(baseX, baseY);
             ctx.fillStyle = random(fillColors);
-            ctx.rotate((getRandom(0, 360) * Math.PI) / 180);
-            ctx.scale(scale, scale);
+            ctx.rotate(randomRotate());
+            ctx.scale(scaleX, scaleY);
             ctx.fill(star);
             ctx.stroke(star);
 
@@ -82,24 +94,32 @@ export default function StarCanvas() {
 
             const leftShift = getRandom(64, 34);
 
-            const leftScale = randomScale(0.2, 0.8);
+            const leftScaleX = randomScale(0.2, 0.8);
+            const leftScaleY = leftScaleX * getRandom(.6,1);
 
             ctx.translate(0 - leftShift, 0);
-            ctx.scale(leftScale, leftScale);
+            const leftRotate = randomRotate();
+            ctx.rotate(leftRotate);
+            ctx.scale(leftScaleX, leftScaleY);
 
             ctx.fillStyle = random(fillColors);
 
             ctx.fill(star);
             ctx.stroke(star);
 
+            ctx.rotate(0 - leftRotate);
+            ctx.scale(1/leftScaleX, 1/leftScaleY);
+
             // draw right star
 
             const rightShift = getRandom(64, 34) + leftShift;
 
-            const rightScale = randomScale(0.2, 0.8) / leftScale;
+            const rightScaleX = randomScale(0.2, 0.8);
+            const rightScaleY = rightScaleX * getRandom(.6,1);
 
             ctx.translate(rightShift, 0);
-            ctx.scale(rightScale, rightScale);
+            ctx.rotate(randomRotate());
+            ctx.scale(rightScaleX, rightScaleY);
 
             ctx.fillStyle = random(fillColors);
 
