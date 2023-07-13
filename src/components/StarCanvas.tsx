aebd1f70d1f0cae9fdb66f27c7d44ab3307452db
Dan Ott <dan@dtott.com>
2023-07-13 09:18:37 -0400

    begin attempting animation

--- src/components/StarCanvas.tsx
+++ src/components/StarCanvas.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState, useLayoutEffect } from 'react';
 import {
   fillColors,
   random,
@@ -65,14 +65,85 @@ function getCurve(steps: number, width:number,height:number, flip: boolean = fal
   return [...LUT1, ...LUT2];
 }
 
+function draw(ctx:CanvasRenderingContext2D, width:number, height:number) {
+  let star = new Path2D(
+    'M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
+  );
+
+    const steps = Math.floor(height * STEP_RATIO);
+
+    const curve = [...getCurve(steps, width, height), ...getCurve(steps, width, height, true)];
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#292524';
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+
+    curve.forEach((point) => {
+      const {x:baseX, y:baseY} = point
+      // const baseX = (point.x / 100) * ((width * 5) / 12);
+      // const baseY = (point.y / 100) * height;
+      const scaleX = randomScale();
+      const scaleY = scaleX * getRandom(.7,1)
+
+      ctx.resetTransform();
+
+      ctx.translate(baseX, baseY);
+      ctx.fillStyle = random(fillColors);
+      ctx.rotate(randomRotate());
+      ctx.scale(scaleX, scaleY);
+      ctx.fill(star);
+      ctx.stroke(star);
+
+      ctx.save();
+
+      // draw left star
+
+      const leftShift = getRandom(64, 34);
+
+      const leftScaleX = randomScale(0.2, 0.8);
+      const leftScaleY = leftScaleX * getRandom(.6,1);
+
+      ctx.translate(0 - leftShift, 0);
+      const leftRotate = randomRotate();
+      ctx.rotate(leftRotate);
+      ctx.scale(leftScaleX, leftScaleY);
+
+      ctx.fillStyle = random(fillColors);
+
+      ctx.fill(star);
+      ctx.stroke(star);
+
+      ctx.restore();
+
+      // ctx.rotate(0 - leftRotate);
+      // ctx.scale(1/leftScaleX, 1/leftScaleY);
+
+      // draw right star
+
+      const rightShift = getRandom(64, 34) + leftShift;
+
+      const rightScaleX = randomScale(0.2, 0.8);
+      const rightScaleY = rightScaleX * getRandom(.6,1);
+
+      ctx.translate(rightShift, 0);
+      ctx.rotate(randomRotate());
+      ctx.scale(rightScaleX, rightScaleY);
+
+      ctx.fillStyle = random(fillColors);
+
+      ctx.fill(star);
+      ctx.stroke(star);
+    });
+
+}
+
 export default function StarCanvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [size, setSize] = useState<{ width: number; height: number }>();
 
   useEffect(() => {
-    let star = new Path2D(
-      'M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
-    );
+
     if (size) {
       const {width,height} = size
       const canvas = canvasRef.current;
@@ -80,79 +151,19 @@ export default function StarCanvas() {
         const ctx = canvas.getContext('2d');
 
         if (ctx) {
-          const steps = Math.floor(height * STEP_RATIO);
-
-          const curve = [...getCurve(steps, width, height), ...getCurve(steps, width, height, true)];
-
-          ctx.lineWidth = 1;
-          ctx.strokeStyle = '#292524';
-          ctx.lineCap = 'round';
-          ctx.lineJoin = 'round';
-
-          curve.forEach((point) => {
-            const {x:baseX, y:baseY} = point
-            // const baseX = (point.x / 100) * ((width * 5) / 12);
-            // const baseY = (point.y / 100) * height;
-            const scaleX = randomScale();
-            const scaleY = scaleX * getRandom(.7,1)
-
-            ctx.resetTransform();
-
-            ctx.translate(baseX, baseY);
-            ctx.fillStyle = random(fillColors);
-            ctx.rotate(randomRotate());
-            ctx.scale(scaleX, scaleY);
-            ctx.fill(star);
-            ctx.stroke(star);
-
-            ctx.save();
-
-            // draw left star
-
-            const leftShift = getRandom(64, 34);
-
-            const leftScaleX = randomScale(0.2, 0.8);
-            const leftScaleY = leftScaleX * getRandom(.6,1);
-
-            ctx.translate(0 - leftShift, 0);
-            const leftRotate = randomRotate();
-            ctx.rotate(leftRotate);
-            ctx.scale(leftScaleX, leftScaleY);
-
-            ctx.fillStyle = random(fillColors);
-
-            ctx.fill(star);
-            ctx.stroke(star);
-
-            ctx.restore();
-
-            // ctx.rotate(0 - leftRotate);
-            // ctx.scale(1/leftScaleX, 1/leftScaleY);
-
-            // draw right star
-
-            const rightShift = getRandom(64, 34) + leftShift;
-
-            const rightScaleX = randomScale(0.2, 0.8);
-            const rightScaleY = rightScaleX * getRandom(.6,1);
-
-            ctx.translate(rightShift, 0);
-            ctx.rotate(randomRotate());
-            ctx.scale(rightScaleX, rightScaleY);
-
-            ctx.fillStyle = random(fillColors);
-
-            ctx.fill(star);
-            ctx.stroke(star);
-          });
+          window.requestAnimationFrame(() => {
+            ctx.clearRect(0, 0, width, height);
+            draw(ctx, width, height);
+          })
         }
       }
     }
   }, [size]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const height = window?.document?.body?.offsetHeight;
     const width = window?.document?.body?.offsetWidth;
+    console.log({height, width})
 
     if (height && width) {
       setSize({ width, height });
