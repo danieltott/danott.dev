fdbdae0797ed87d665496ab595edefbe0064bf35
Dan Ott <dan@dtott.com>
2023-07-13 11:52:06 -0400

    remove alpha stuff

--- src/components/StarCanvas.tsx
+++ src/components/StarCanvas.tsx
@@ -94,10 +94,7 @@ function draw(ctx: CanvasRenderingContext2D, width: number, height: number) {
     'M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
   );
 
-  let startingAlpha = 0;
 
-  function drawAllStars(alpha: number) {
-    ctx.globalAlpha = alpha;
 
     ctx.clearRect(0, 0, width, height);
 
@@ -179,16 +176,6 @@ function draw(ctx: CanvasRenderingContext2D, width: number, height: number) {
       drawStar(0);
     });
 
-    // if(alpha < 1) {
-    //   window.requestAnimationFrame(() => {
-    //     drawAllStars(alpha + 0.01);
-    //   });
-    // }
-  }
-
-  window.requestAnimationFrame(() => {
-    drawAllStars(1);
-  });
 
   // const baseX = (point.x / 100) * ((width * 5) / 12);
   // const baseY = (point.y / 100) * height;
