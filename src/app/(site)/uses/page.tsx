a818afa698e90c044083bc6de61fd4887312fe6a
Dan Ott <dan@dtott.com>
2023-07-19 11:18:03 -0400

    Add Star to link callout

--- src/app/(site)/uses/page.tsx
+++ src/app/(site)/uses/page.tsx
@@ -1,6 +1,7 @@
 import { Card } from '@/components/Card';
 import { Section } from '@/components/Section';
 import { SimpleLayout } from '@/components/SimpleLayout';
+import Star from '@/components/Star';
 import type { LinkHref } from '@/lib/types';
 import { type Metadata } from 'next';
 import Link from 'next/link';
@@ -11,7 +12,7 @@ function ToolsSection({
 }: React.ComponentProps<typeof Section>) {
   return (
     <Section {...props}>
-      <ul role="list" className="space-y-16">
+      <ul role="list" className="grid gap-16">
         {children}
       </ul>
     </Section>
@@ -100,7 +101,9 @@ export default function Uses() {
               </p>
             </Tool>
             <Tool title="Konva" href="https://konvajs.org/docs/react/">
-              <p>Generate and animate the stars via canvas.</p>
+              <p>
+                Generate the stars via <code>&lt;canvas&gt;</code>.
+              </p>
             </Tool>
             <Tool
               title={
@@ -121,6 +124,33 @@ export default function Uses() {
             <Tool title="Prettier" href="https://prettier.io/">
               Don&rsquo;t leave home without it!
             </Tool>
+
+            <li className="flex items-center gap-2 group">
+              <Star className="fill-green-metal-300 group-hover:animate-bounce" />
+              {/* <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                className="w-6 h-6 stroke-stone-800 fill-green-metal-300 group-hover:animate-bounce"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                />
+              </svg> */}
+
+              <div>
+                Read more about how I built this site in my{` `}
+                <Link
+                  className="text-link"
+                  href="/thoughts/new-site-danott-dev"
+                >
+                  blog post
+                </Link>
+                !
+              </div>
+            </li>
           </ToolsSection>
           <ToolsSection title="Hardware">
             <Tool title={<>MacBook Pro 13&rdquo;, M1, 2020</>}>
